a59ebf900f71108b08f662c28ae200b8eb7908ca
lzaytseva <lvzaytseva1@gmail.com>
2022-12-27 18:30:12 +0300

    String заменен на StringBuilder

--- src/main/java/Utils/TextFormatUtils.java
+++ src/main/java/Utils/TextFormatUtils.java
@@ -2,23 +2,24 @@ package Utils;
 
 public class TextFormatUtils {
     public static String getCorrectFormOfWord(double price) {
-        String result = "рубл";
+        StringBuilder result = new StringBuilder("рубл");
+
         int roundedPriceLastDigits = (int) Math.floor(price) % 100;
 
         if (roundedPriceLastDigits >= 11 && roundedPriceLastDigits <= 14) {
-            result += "ей";
+            result.append("ей");
         } else {
             roundedPriceLastDigits %= 10;
 
             if (roundedPriceLastDigits == 1) {
-                result += "ь";
+                result.append("ь");
             } else if (roundedPriceLastDigits >= 2 && roundedPriceLastDigits <= 4) {
-                result += "я";
+                result.append("я");
             } else {
-                result += "ей";
+                result.append("ей");
             }
         }
 
-        return result;
+        return result.toString();
     }
 }
